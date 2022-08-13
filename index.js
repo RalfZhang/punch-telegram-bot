2eda80e4cddc6bba66e56cd4959690f87c049cee
RalfZhang <zhangkang103@gmail.com>
2022-08-13 14:10:12 -0400

    feat: update to punch No.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const TelegramBot = require('node-telegram-bot-api');
+const dateFormat = require('dayjs');
 const sqlConfig = require('./sql-config');
-const { token, groupId } = require('./app-config');
+const { token } = require('./app-config');
 
 const bot = new TelegramBot(token, {polling: true});
 
 const knex = require('knex')(sqlConfig);
+
+const isTriedToday = async (id, num, time) => {
+  const timeObj = new Date(time * 1000);
+  const nextTimeObj = new Date((time + 24 * 60 * 60) * 1000);
+  const from = dateFormat(timeObj).format('YYYY-MM-DD');
+  const to = dateFormat(nextTimeObj).format('YYYY-MM-DD');
+
+  const res = await knex('check')
+    .select()
+    .where({
+      tg_id: id,
+      num: num,
+    })
+    .whereBetween('time', [from, to])
+    .first();
+  // console.log('istried', res);
+  return !!res;
+}
 
 const save = (id, num, time) => {
   return knex('check').insert({
@@ -15,11 +34,18 @@ const save = (id, num, time) => {
 }
 
 const get = async (id) => {
-  const res = await knex('check').where({
-    tg_id: id,
-  }).sum('num as n');
+  const subQuery = knex
+    .select('num')
+    .from('check')
+    .where('tg_id', '=', id)
+    .groupBy('num')
+    .as('t1');
 
-  return res[0].n;
+  const res = await knex
+    .count('* as count')
+    .from(subQuery)
+    .first();
+  return res.count;
 }
 
 // Matches "/echo [whatever]"
@@ -40,13 +66,13 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 // messages.
 bot.on('message', async (msg) => {
   try {
-    console.log('msg----\n', msg);
+    // console.log('msg----\n', msg);
     const chatId = msg.chat.id;
-    if (chatId !== groupId) {
-      bot.sendMessage(chatId, `不让你用，除非打钱`);
+    // if (chatId !== groupId) {
+    //   bot.sendMessage(chatId, `不让你用，除非打钱`);
 
-      return;
-    }
+    //   return;
+    // }
     if (!msg.from.id) {
       bot.sendMessage(chatId, `你是谁？`);
       return;
@@ -60,6 +86,11 @@ bot.on('message', async (msg) => {
       const num = numRegRes && numRegRes[2] && +numRegRes[2];
       if (!num) {
         bot.sendMessage(chatId, `/punch 后面只能给我正确的数字！`);
+        return;
+      }
+      const isTried = await isTriedToday(uid, +num, msg.date);
+      if (isTried) {
+        bot.sendMessage(chatId, `你今天已经练过 ${num} 了！`);
         return;
       }
 
